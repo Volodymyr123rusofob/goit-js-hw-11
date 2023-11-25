8b200e23645b150628b3c2f18cd3b2729e920093
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-11-26 00:04:01 +0200

    task-4-11

--- src/example.js
+++ src/example.js
@@ -2,20 +2,34 @@ import axios from 'axios';
 export class RequestOnColechtion {
   #URL = 'https://pixabay.com/api/';
   #KEY = '40816428-8ff543c0077c3bf6c3247c305';
+  #page = 1;
   #params = new URLSearchParams({
     key: `${this.#KEY}`,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    per_page: 40,
   });
 
   async requestColechtion(value) {
-    const url = `${this.#URL}?${this.#params}&q=${value}`;
+    const url = `${this.#URL}?${this.#params}&q=${value}&page=${this.#page}`;
     try {
       const { data } = await axios(url);
       return data;
     } catch (error) {
       console.error(error.message);
     }
+  }
+  async requestNextColechtion(value) {
+    const url = `${this.#URL}?${this.#params}&q=${value}&page=${this.#page}`;
+    try {
+      const { data } = await axios(url);
+      return data;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  set page(newPage) {
+    this.#page = newPage;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import { RequestOnColechtion } from './example';
 import { Notify } from 'notiflix';
 
-const divGallery = document.querySelector('.gallery');
-const formSerch = document.querySelector('.search-form');
-const api = new RequestOnColechtion();
+const ref = {
+  btnLoad: document.querySelector('.load-more'),
+  divGallery: document.querySelector('.gallery'),
+  formSerch: document.querySelector('.search-form'),
+  api: new RequestOnColechtion(),
+};
+
+const { divGallery, formSerch, api, btnLoad } = ref;
+let userValue = '';
 
 formSerch.addEventListener('submit', e => {
   e.preventDefault();
-  const userValue = e.target[0].value;
+  userValue = e.target[0].value;
   if (!userValue) return Notify.warning('EThe field must not be empty');
   api.requestColechtion(userValue).then(data => {
     const arrBek = data.hits;
@@ -16,8 +22,24 @@ formSerch.addEventListener('submit', e => {
         'Sorry, there are no images matching your search query. Please try again.'
       );
     renderImg(arrBek);
+    btnLoad.style.display = 'block';
   });
+
   e.target[0].value = '';
+});
+
+btnLoad.addEventListener('click', () => {
+  console.log();
+  api.page += 1;
+  console.log(userValue);
+  api.requestNextColechtion(userValue).then(data => {
+    const arrBek = data.hits;
+    if (arrBek.length === 0)
+      return Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    renderImg(arrBek);
+  });
 });
 
 function renderImg(arr) {
@@ -36,19 +58,21 @@ function renderImg(arr) {
   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes${likes}</b>
+      <b>Likes: ${likes}</b>
     </p>
     <p class="info-item">
-      <b>Views${views}</b>
+      <b>Views: ${views}</b>
     </p>
     <p class="info-item">
-      <b>Comments${comments}</b>
+      <b>Comments: ${comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads${downloads}</b>
+      <b>Downloads: ${downloads}</b>
     </p>
   </div>
 </div>`);
   });
   divGallery.innerHTML = galleryRender;
 }
+// per_page - кількість фото на сторінці.
+// page - кількість сторінок по per_page штук
