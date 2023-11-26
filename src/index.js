196ca1268fac241970b71f6176f09d2094b01b65
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-11-26 23:52:57 +0200

    task-5-11

--- src/index.js
+++ src/index.js
@@ -1,23 +1,125 @@
+// import { RequestOnColechtion } from './example';
+// import { Notify } from 'notiflix';
+// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// const ref = {
+//   btnLoad: document.querySelector('.load-more'),
+//   divGallery: document.querySelector('.galleryU'),
+//   formSerch: document.querySelector('.search-form'),
+//   api: new RequestOnColechtion(),
+// };
+
+// const { divGallery, formSerch, api, btnLoad } = ref;
+// let userValue = '';
+// let number = 1;
+// let totalImg = 0;
+
+// formSerch.addEventListener('submit', e => {
+//   e.preventDefault();
+//   btnLoad.style.display = 'none';
+//   api.page = '1';
+//   number = 1;
+//   divGallery.innerHTML = '';
+//   userValue = e.target[0].value;
+
+//   if (!userValue) return Notify.warning('EThe field must not be empty');
+
+//   api.requestColechtion(userValue).then(data => {
+//     const arrBek = data.hits;
+//     const { totalHits } = data;
+//     Notify.success(`Hooray! We found ${totalHits} images.`);
+//     totalImg = arrBek.length;
+
+//     if (arrBek.length === 0)
+//       return Notify.info(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//     renderImg(arrBek);
+//     if (totalImg < 40) {
+//       btnLoad.style.display = 'none';
+//       return Notify.info(
+//         "We're sorry, but you've reached the end of search results."
+//       );
+//     }
+//     btnLoad.style.display = 'block';
+//     new SimpleLightbox('.gallery a');
+//   });
+//   e.target[0].value = '';
+// });
+
+// btnLoad.addEventListener('click', () => {
+//   number += 1;
+//   api.page = number.toString();
+//   api.requestNextColechtion(userValue).then(data => {
+//     const { totalHits } = data;
+//     const arrBek = data.hits;
+//     totalImg += arrBek.length;
+//     if (arrBek.length === 0)
+//       return Notify.info(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//     renderImg(arrBek);
+//     const simple = new SimpleLightbox('.gallery a');
+//     simple.refresh();
+//     if (totalImg === totalHits) {
+//       btnLoad.style.display = 'none';
+//       return Notify.info(
+//         "We're sorry, but you've reached the end of search results."
+//       );
+//     }
+//   });
+// });
+
+// function renderImg(arr) {
+//   const galleryRender = [];
+//   arr.forEach(e => {
+//     const {
+//       webformatURL,
+//       tags,
+//       likes,
+//       views,
+//       comments,
+//       downloads,
+//       largeImageURL,
+//     } = e;
+//     galleryRender.push(`<li class="gallery">
+//     <a class="photo-card-lin" href="${largeImageURL}">
+//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//   </a>
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes: ${likes}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views: ${views}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments: ${comments}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads: ${downloads}</b>
+//     </p>
+//   </div>
+// </li>`);
+//   });
+//   divGallery.insertAdjacentHTML('beforeend', galleryRender);
+// }
+// !!!!!!!!!!!!!!!!!
 import { RequestOnColechtion } from './example';
 import { Notify } from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const ref = {
   btnLoad: document.querySelector('.load-more'),
-  divGallery: document.querySelector('.gallery'),
-  divImgCard: document.querySelector('.photo-card'),
+  divGallery: document.querySelector('.galleryU'),
   formSerch: document.querySelector('.search-form'),
   api: new RequestOnColechtion(),
 };
 
-const { divGallery, formSerch, api, btnLoad, divImgCard } = ref;
+const { divGallery, formSerch, api, btnLoad } = ref;
 let userValue = '';
 let number = 1;
 let totalImg = 0;
-const simpLayt = new SimpleLightbox('.photo-card a', {
-  captionDelay: 250,
-});
 
 formSerch.addEventListener('submit', e => {
   e.preventDefault();
@@ -26,17 +128,21 @@ formSerch.addEventListener('submit', e => {
   number = 1;
   divGallery.innerHTML = '';
   userValue = e.target[0].value;
+
   if (!userValue) return Notify.warning('EThe field must not be empty');
+
   api.requestColechtion(userValue).then(data => {
     const arrBek = data.hits;
     const { totalHits } = data;
     Notify.success(`Hooray! We found ${totalHits} images.`);
     totalImg = arrBek.length;
+
     if (arrBek.length === 0)
       return Notify.info(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     renderImg(arrBek);
+
     if (totalImg < 40) {
       btnLoad.style.display = 'none';
       return Notify.info(
@@ -44,6 +150,7 @@ formSerch.addEventListener('submit', e => {
       );
     }
     btnLoad.style.display = 'block';
+    api.updateLightbox();
   });
   e.target[0].value = '';
 });
@@ -60,7 +167,7 @@ btnLoad.addEventListener('click', () => {
         'Sorry, there are no images matching your search query. Please try again.'
       );
     renderImg(arrBek);
-    simpLayt.refresh();
+    api.updateLightbox();
     if (totalImg === totalHits) {
       btnLoad.style.display = 'none';
       return Notify.info(
@@ -82,9 +189,10 @@ function renderImg(arr) {
       downloads,
       largeImageURL,
     } = e;
-    galleryRender.push(`<div class="photo-card">
-    
-  <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+    galleryRender.push(`<li class="gallery">
+    <a class="photo-card-lin" href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  </a>
   <div class="info">
     <p class="info-item">
       <b>Likes: ${likes}</b>
@@ -99,8 +207,7 @@ function renderImg(arr) {
       <b>Downloads: ${downloads}</b>
     </p>
   </div>
-</div>`);
+</li>`);
   });
   divGallery.insertAdjacentHTML('beforeend', galleryRender);
-  simpLayt;
 }
